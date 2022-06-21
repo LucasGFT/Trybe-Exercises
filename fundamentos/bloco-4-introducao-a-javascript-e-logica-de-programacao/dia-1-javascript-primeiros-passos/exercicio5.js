fc5880f4f4de203f9c1d988d325849b5254e66c1
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-06-21 18:14:27 -0300

    resolvido exercicio 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,11 @@
+const a = 60;
+const b = 50;
+const c = 70;
+
+if (a + b + c == 180) {
+  console.log("true");
+} else if (a || b || c <= 0) {
+  console.log("Angulo incorreto");
+} else {
+  console.log("false");
+}
