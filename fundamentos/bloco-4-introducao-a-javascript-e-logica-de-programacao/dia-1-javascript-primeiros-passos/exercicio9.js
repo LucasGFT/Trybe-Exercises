f2fd757a171f6cf23daee72be1041809f13508b0
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-06-21 18:45:25 -0300

    resolvido exercicio9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,9 @@
+const a = 12;
+const b = 26;
+const c = 13;
+
+if (a % 2 == !0 || b % 2 == !0 || c % 2 == !0) {
+  console.log("Um dos três numeros são impar");
+} else {
+  console.log("Nenhum numero é impar");
+}
