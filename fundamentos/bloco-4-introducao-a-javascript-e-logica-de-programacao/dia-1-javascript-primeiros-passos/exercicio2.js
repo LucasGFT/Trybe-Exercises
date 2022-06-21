819dd2391a24d107d632016deb1304b8ac20bc63
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-06-21 18:04:47 -0300

    Resolvido exercicio 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,8 @@
+const a = 20;
+const b = 30;
+
+if (a > b) {
+  console.log("O maior numero é a =", a);
+} else {
+  console.log("O maior numero é b =", b);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 160;
+const b = 340;
+const c = 250;
+
+if (a > b && a > c) {
+  console.log("O maior numero é a =", a);
+} else if (b > a && b > c) {
+  console.log("O maior numero é b =", b);
+} else if (c > a && c > b) {
+  console.log("O maior numero é c =", c);
+}
