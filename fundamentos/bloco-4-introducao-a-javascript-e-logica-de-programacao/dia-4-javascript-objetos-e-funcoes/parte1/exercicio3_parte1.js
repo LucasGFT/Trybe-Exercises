4e1c5417c52a77b2ed46f7437544cf11970b5b5f
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-06-24 18:16:07 -0300

    Resolvido parte 1 dos exercicios/4.4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1/exercicio3_parte1.js
@@ -0,0 +1,12 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+//Faça um for/in que mostre todas as chaves do objeto.
+
+info.recorrente = "Sim";
+
+for (i in info) {
+  console.log(i);
+}
